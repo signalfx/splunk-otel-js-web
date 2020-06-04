748e3380d1506a2e0a185a557c2856234a750275
John Bley <jbley@splunk.com>
2020-06-04 13:39:20 -0400

    Add real tests for init() scenarios.

--- src/sfx-rum.js
+++ src/sfx-rum.js
@@ -7,138 +7,145 @@ import {hrTimeToMicroseconds} from '@opentelemetry/core';
 
 console.log('hi');
 if (!window.SfxRum) {
-    window.SfxRum = {};
-    // FIXME learn how to produce docs for 'exported' items (init and its options)
-    // options.beaconUrl (example format: 'http://127.0.0.1:9080/api/v2/spans'
-    window.SfxRum.init = function (options) {
-        if (!options.beaconUrl) {
-            // FIXME error handling
-            console.log("init( {beaconUrl: 'https://something'} ) is required.");
-            return;
+  window.SfxRum = {
+    inited: false
+  };
+  // FIXME learn how to produce docs for 'exported' items (init and its options)
+  // options.beaconUrl (example format: 'http://127.0.0.1:9080/api/v2/spans'
+  window.SfxRum.init = function (options) {
+    if (this.inited) {
+      console.log("already init()ed.");
+      return;
+    }
+    if (!options.beaconUrl) {
+      // FIXME error handling
+      console.log("init( {beaconUrl: 'https://something'} ) is required.");
+      return;
+    }
+    console.log('SfxRum.init() starting');
+
+    const exportUrl = options.beaconUrl;
+
+
+    if (!document.cookie.includes("rumSessionID")) {
+      var id = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx".replace(/x/g, function (c) {
+        return ((Math.random() * 16) | 0).toString(16);
+      });
+      document.cookie = "rumSessionID=" + id + "; path=/";
+    }
+    var rumSessionId = function () {
+      console.log("doc.cookie is " + document.cookie);
+      var decodedCookie = decodeURIComponent(document.cookie);
+      var cookies = decodedCookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+        var c = cookies[i].trim();
+        if (c.indexOf("rumSessionID=") === 0) {
+          return c.substring("rumSessionID=".length, c.length);
         }
-        console.log('SfxRum.init() starting');
+      }
+      return undefined;
+    }();
 
-        const exportUrl = options.beaconUrl;
+    const provider = new WebTracerProvider({
+      plugins: [
+        new DocumentLoad(),
+        new XMLHttpRequestPlugin(),
+      ],
+      defaultAttributes: {
+        'sfx.rumSessionId': rumSessionId
+      }
+    });
 
 
-        if (!document.cookie.includes("rumSessionID")) {
-            var id = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx".replace(/x/g, function (c) {
-                return ((Math.random() * 16) | 0).toString(16);
-            });
-            document.cookie = "rumSessionID=" + id + "; path=/";
+    // FIXME this is a nasty copy+paste hack to get the zipkin exporter able to work in a browser context.
+    class MySpanExporter {
+      constructor() {
+      }
+
+      export(spans, resultCallback) {
+        const zspans = spans.map(span => this.toZipkinSpan(span, 'browser'));
+        const zJson = JSON.stringify(zspans);
+        const useBeacon = true;
+        if (useBeacon) {
+          navigator.sendBeacon(exportUrl, zJson);
+        } else { // FIXME causes CORS problems typically - figure out how it's supposed to work long-term
+          const xhr = new XMLHttpRequest();
+          xhr.open('POST', exportUrl);
+          //xhr.setRequestHeader(collectorTypes.OT_REQUEST_HEADER, '1');
+          xhr.setRequestHeader('Accept', 'application/json');
+          xhr.setRequestHeader('Content-Type', 'application/json');
+          xhr.send(zJson);
+          // don't bother waiting for answer
         }
-        var rumSessionId = function () {
-            console.log("doc.cookie is " + document.cookie);
-            var decodedCookie = decodeURIComponent(document.cookie);
-            var cookies = decodedCookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var c = cookies[i].trim();
-                if (c.indexOf("rumSessionID=") === 0) {
-                    return c.substring("rumSessionID=".length, c.length);
-                }
-            }
-            return undefined;
-        }();
+        resultCallback("SUCCESS");
+      }
 
-        const provider = new WebTracerProvider({
-            plugins: [
-                new DocumentLoad(),
-                new XMLHttpRequestPlugin(),
-            ],
-            defaultAttributes: {
-                'sfx.rumSessionId': rumSessionId
-            }
-        });
+      shutdown() {
+      }
 
 
-        // FIXME this is a nasty copy+paste hack to get the zipkin exporter able to work in a browser context.
-        class MySpanExporter {
-            constructor() {
-            }
-
-            export(spans, resultCallback) {
-                const zspans = spans.map(span => this.toZipkinSpan(span, 'browser'));
-                const zJson = JSON.stringify(zspans);
-                const useBeacon = true;
-                if (useBeacon) {
-                    navigator.sendBeacon(exportUrl, zJson);
-                } else { // FIXME causes CORS problems typically - figure out how it's supposed to work long-term
-                    const xhr = new XMLHttpRequest();
-                    xhr.open('POST', exportUrl);
-                    //xhr.setRequestHeader(collectorTypes.OT_REQUEST_HEADER, '1');
-                    xhr.setRequestHeader('Accept', 'application/json');
-                    xhr.setRequestHeader('Content-Type', 'application/json');
-                    xhr.send(zJson);
-                    // don't bother waiting for answer
-                }
-                resultCallback("SUCCESS");
-            }
-
-            shutdown() {
-            }
-
-
-            toZipkinTags(attrs, status, statusCodeTagName, statusDescriptionTagName, resource) {
-                const tags = {};
-                for (const key of Object.keys(attrs)) {
-                    tags[key] = String(attrs[key]);
-                }
-                tags[statusCodeTagName] = String(api.CanonicalCode[status.code]);
-                if (status.message) {
-                    tags[statusDescriptionTagName] = status.message;
-                }
-                Object.keys(resource.labels).forEach(
-                    name => (tags[name] = resource.labels[name])
-                );
-                tags['sfx.rumSessionId'] = rumSessionId;
-                return tags;
-            }
-
-            toZipkinAnnotations(events) {
-                return events.map(event => ({
-                    timestamp: hrTimeToMicroseconds(event.time),
-                    value: event.name,
-                }));
-            }
-
-            toZipkinSpan(span, serviceName) {
-                const ZIPKIN_SPAN_KIND_MAPPING = {
-                    [api.SpanKind.CLIENT]: 'CLIENT',
-                    [api.SpanKind.SERVER]: 'SERVER',
-                    [api.SpanKind.CONSUMER]: 'CONSUMER',
-                    [api.SpanKind.PRODUCER]: 'PRODUCER',
-                    // When absent, the span is local.
-                    [api.SpanKind.INTERNAL]: undefined,
-                };
-                return {
-                    traceId: span.spanContext.traceId,
-                    parentId: span.parentSpanId,
-                    name: span.name,
-                    id: span.spanContext.spanId,
-                    kind: ZIPKIN_SPAN_KIND_MAPPING[span.kind],
-                    timestamp: hrTimeToMicroseconds(span.startTime),
-                    duration: hrTimeToMicroseconds(span.duration),
-                    localEndpoint: {serviceName},
-                    tags: this.toZipkinTags(
-                        span.attributes,
-                        span.status,
-                        'ot.status_code',
-                        'ot.status_description',
-                        span.resource
-                    ),
-                    annotations: span.events.length
-                        ? this.toZipkinAnnotations(span.events)
-                        : undefined,
-                };
-            }
-
-
+      toZipkinTags(attrs, status, statusCodeTagName, statusDescriptionTagName, resource) {
+        const tags = {};
+        for (const key of Object.keys(attrs)) {
+          tags[key] = String(attrs[key]);
         }
+        tags[statusCodeTagName] = String(api.CanonicalCode[status.code]);
+        if (status.message) {
+          tags[statusDescriptionTagName] = status.message;
+        }
+        Object.keys(resource.labels).forEach(
+          name => (tags[name] = resource.labels[name])
+        );
+        tags['sfx.rumSessionId'] = rumSessionId;
+        return tags;
+      }
 
-        provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
-        provider.addSpanProcessor(new SimpleSpanProcessor(new MySpanExporter()));
-        provider.register();
-        console.log('SfxRum.init() complete');
+      toZipkinAnnotations(events) {
+        return events.map(event => ({
+          timestamp: hrTimeToMicroseconds(event.time),
+          value: event.name,
+        }));
+      }
 
-    };
+      toZipkinSpan(span, serviceName) {
+        const ZIPKIN_SPAN_KIND_MAPPING = {
+          [api.SpanKind.CLIENT]: 'CLIENT',
+          [api.SpanKind.SERVER]: 'SERVER',
+          [api.SpanKind.CONSUMER]: 'CONSUMER',
+          [api.SpanKind.PRODUCER]: 'PRODUCER',
+          // When absent, the span is local.
+          [api.SpanKind.INTERNAL]: undefined,
+        };
+        return {
+          traceId: span.spanContext.traceId,
+          parentId: span.parentSpanId,
+          name: span.name,
+          id: span.spanContext.spanId,
+          kind: ZIPKIN_SPAN_KIND_MAPPING[span.kind],
+          timestamp: hrTimeToMicroseconds(span.startTime),
+          duration: hrTimeToMicroseconds(span.duration),
+          localEndpoint: {serviceName},
+          tags: this.toZipkinTags(
+            span.attributes,
+            span.status,
+            'ot.status_code',
+            'ot.status_description',
+            span.resource
+          ),
+          annotations: span.events.length
+            ? this.toZipkinAnnotations(span.events)
+            : undefined,
+        };
+      }
+
+
+    }
+
+    provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+    provider.addSpanProcessor(new SimpleSpanProcessor(new MySpanExporter()));
+    provider.register();
+    this.inited = true;
+    console.log('SfxRum.init() complete');
+
+  };
 }
--- test/init.test.js
+++ test/init.test.js
@@ -3,10 +3,24 @@ import * as api from '@opentelemetry/api';
 
 require('../src/sfx-rum.js');
 
-describe('test setup', () => {
-  window.SfxRum.init( {beaconUrl: 'http://127.0.0.1:9999/foo' } );
-  console.log('OK!');
-  it ('should have created a session cookie', () => {
-    assert.ok(document.cookie.includes("rumSessionID"));
- });
+describe('test init', () => {
+  describe('not specifying beaconUrl', () => {
+    it('should not be inited', () => {
+      window.SfxRum.init({noBeaconUrl: true});
+      assert.ok(window.SfxRum.inited === false);
+    });
+  });
+  describe('successful', () => {
+    it('should have been inited and created a session cookie', () => {
+      window.SfxRum.init({beaconUrl: 'http://127.0.0.1:9999/foo'});
+      assert.ok(window.SfxRum.inited);
+      assert.ok(document.cookie.includes("rumSessionID"));
+    });
+  });
+  describe('double-init has no effect', () => {
+    it('should have been inited only once', () => {
+      window.SfxRum.init({beaconUrl: 'http://127.0.0.1:8888/bar'});
+      // FIXME way to check this state (e.g., capture beaconUrl and ensure it hasn't changed
+    });
+  });
 });
