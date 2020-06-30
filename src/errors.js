4d6ca6d57286421bea2704124d316003ef9290ca
John Bley <jbley@splunk.com>
2020-06-30 07:25:12 -0400

    Drop TraceKit and rework away from it.

--- src/errors.js
+++ src/errors.js
@@ -1,118 +1,116 @@
-// FIXME does this pollute window.Tracekit?  Can I fix that?
-import Tracekit from 'tracekit';
 import shimmer from 'shimmer';
 
 const STACK_LIMIT = 4096;
-
-function stackToString(stack) {
-  let answer = '';
-  for(const frame of stack) {
-    const line = frame.line || '';
-    const col = frame.column || '';
-    const f = frame.func || '<?>'
-    const linePlusCol = (frame.line && frame.column) ? `:${line}:${col}` : '';
-    answer += `  at ${f} (${frame.url}${linePlusCol})\n`;
-    if (answer.length > STACK_LIMIT) {
-      return answer.substr(0, STACK_LIMIT-3)+'...';
-    }
-  }
-  return answer;
-}
+const MESSAGE_LIMIT = 1024;
 
 function useful(s) {
   return s && s.trim() !== '' && !s.startsWith('[object');
 }
 
-function addStackIfUseful(span, report) {
-  if (report.stack) {
-    const stackString = stackToString(report.stack);
-    if (useful(stackString)) {
-      span.setAttribute('error.stack', stackString);
-    }
+function limit(s, limit) {
+  if (s.length > limit) {
+    return s.substring(0, limit);
+  } else {
+    return s;
   }
 }
 
-function handleTracekitError(tracer, report) {
-  if (!useful(report.name) || !useful(report.message)) {
-    return;
+function addStackIfUseful(span, err) {
+  // FIXME handle Map<> form of err.stack
+  if (err && err.stack && useful(err.stack)) {
+    span.setAttribute('error.stack', limit(err.stack, STACK_LIMIT));
   }
-  const span = tracer.startSpan(report.name);
-  span.setAttribute('component', 'error');
-  span.setAttribute('error.type', report.name);
-  span.setAttribute('error.message', report.message);
-  addStackIfUseful(span, report);
-  span.end(span.startTime);
 }
 
-function registerTracekitListener(tracer) {
-  Tracekit.remoteFetching = false; // don't try to get SourceMaps, etc.
-  Tracekit.report.subscribe(function(report) {
-    handleTracekitError(tracer, report);
-  });
-}
-
-function stringifyArgs(...args) {
-  return args.map(o => o.toString()).join(' ');
-}
-
-// FIXME clean up some of the repetition in here.. one unified 'string or string array or error object' input
-function handleErrorMessage(tracer, type, msg) {
-  if (!useful(msg)) {
-    return;
-  }
-  const span = tracer.startSpan(name);
-  span.setAttribute('component', 'error');
-  span.setAttribute('error.type', type);
-  span.setAttribute('error.message', msg);
-  // FIXME compute and send stack trace?
-  span.end(span.startTime);
-}
-
-function handleErrorError(tracer, type, err) {
-  const report = Tracekit.computeStackTrace(err);
-  const msg = err.message || err.toString();
-  if (!useful(msg) && (!report || !report.stack)) {
-    return;
-  }
-  const span = tracer.startSpan(name);
-  span.setAttribute('component', 'error');
-  span.setAttribute('error.type', type);
-  span.setAttribute('error.message', msg);
-  addStackIfUseful(span, report);
-  span.end(span.startTime);
-}
-
-function captureConsoleError(tracer) {
+function captureConsoleError(reporter) {
   shimmer.wrap(console, 'error', function(original) {
     return function () {
-      if (arguments.length === 1 && arguments[0] instanceof Error) {
-        handleErrorError(tracer, 'console.error', arguments[0]);
-      } else {
-        const message = stringifyArgs(Array.from(arguments));
-        handleErrorMessage(tracer, 'console.error', message);
-      }
+      reporter.report('console.error', Array.from(arguments));
       return original.apply(this, arguments);
     };
   });
 }
 
-function registerUnhandledRejectionListener(tracer) {
+function registerUnhandledRejectionListener(reporter) {
   window.addEventListener('unhandledrejection', function (event) {
-    if (event.reason instanceof Error) {
-      handleErrorError(tracer, 'unhandledrejection', event.reason);
-    } else {
-      const msg = event.reason.toString(); // FIXME should be using JSON.stringify here and elsewhere?
-      handleErrorMessage(tracer, 'unhandledrejection', msg);
-    }
+    reporter.report('unhandledrejection', event.reason);
   });
 }
 
-export function captureErrors(provider) {
+function captureError(reporter) {
+  window.addEventListener('error', function(event) {
+    reporter.report('onerror', event);
+  });
+}
+
+// FIXME limit length of error.message
+class ErrorReporter {
+  constructor(tracer) {
+    this.tracer = tracer;
+  }
+  reportError(source, err) {
+    const msg = err.message || err.toString();
+    if (!useful(msg) && (!err.stack)) {
+      return;
+    }
+    const span = this.tracer.startSpan(source);
+    span.setAttribute('component', 'error');
+    span.setAttribute('error.object', useful(err.name) ? err.name : (err.constructor && err.constructor.name ? err.constructor.name : 'Error'));
+    span.setAttribute('error.message', limit(msg, MESSAGE_LIMIT));
+    addStackIfUseful(span, err);
+    span.end(span.startTime);
+
+  }
+  reportString(source, s) {
+    if (!useful(s)) {
+      return;
+    }
+    const span = this.tracer.startSpan(source);
+    span.setAttribute('component', 'error');
+    span.setAttribute('error.object', 'String');
+    span.setAttribute('error.message', limit(s, MESSAGE_LIMIT));
+    // FIXME compute and send stack trace?
+    span.end(span.startTime);
+  }
+  reportErrorEvent(source, ev) {
+    if (ev.error) {
+      this.report(source, ev.error);
+    } else if (ev.message) {
+      this.report(source, ev.message);
+    }
+  }
+
+  report(source, arg) {
+    if (!arg) {
+      return;
+    }
+    if (arg instanceof Array && arg.length == 1) {
+      arg = arg[0];
+    }
+    if (arg instanceof Error) {
+      this.reportError(source, arg);
+    } else if (arg instanceof ErrorEvent) {
+      this.reportErrorEvent(source, arg);
+    } else if (arg instanceof String) {
+      this.reportString(source, arg);
+    } else if (arg instanceof Array) {
+      this.reportString(source, arg.map(x => x.toString()).join(' '));
+    } else {
+      this.reportString(source, arg.toString()); // FIXME or JSON.stringify?
+    }
+  }
+}
+
+
+export function captureErrors(sfxRum, provider) {
   const tracer = provider.getTracer('error');
-  registerTracekitListener(tracer);
-  captureConsoleError(tracer);
-  registerUnhandledRejectionListener(tracer);
+  const reporter = new ErrorReporter(tracer);
+  captureError(reporter);
+  captureConsoleError(reporter);
+  registerUnhandledRejectionListener(reporter);
+  sfxRum.error = function() {
+    reporter.report('SfxRum.error', Array.from(arguments));
+  }
 
   // Future possibility is https://www.w3.org/TR/reporting/#reporting-observer
-  // FIXME expose SfxRum.report(err)
 }
