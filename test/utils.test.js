2ab21b0a1f7966001055b3ef32ad5c906d91198a
John Bley <jbley@splunk.com>
2020-07-20 19:52:36 -0400

    Factor ID generation out into new utils method

---
+++ test/utils.test.js
@@ -0,0 +1,13 @@
+import * as assert from 'assert';
+import {generateId} from '../src/utils';
+
+describe('test generateId', () => {
+  it('should generate IDs of 64 and 128 bits', () => {
+    const id64 = generateId(64);
+    const id128 = generateId(128);
+    assert.ok(id64.length === 16);
+    assert.ok(id128.length === 32);
+    assert.ok(id64.match('^[0-9a-z]+$'));
+    assert.ok(id128.match('^[0-9a-z]+$'));
+  });
+});
