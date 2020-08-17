aac177e32c50f7e86363717b6031585c1b5c2a81
John Bley <jbley@splunk.com>
2020-08-17 14:49:18 -0400

    Turn back on FireFox tests

--- karma.conf.js
+++ karma.conf.js
@@ -76,8 +76,8 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    //        browsers: ['Chrome', 'Firefox'],
-    browsers: ['Chrome'], // useful to speed up edit+test cycle
+    browsers: ['Chrome', 'Firefox'],
+    //browsers: ['Chrome'], // useful to speed up edit+test cycle
 
 
     // Continuous Integration mode
