2622dfd51536cd0d2a3438ac031cfd589c28e4e2
John Bley <jbley@splunk.com>
2020-09-17 11:34:59 -0400

    Proper SUCCESS token for the exporter. This matters if you're hooked up to the BatchSpanProcessor which will generate Promise rejections if the proper one is not used, which will cause error spans for the unhandled promise rejection, which will cause another rejected promise...

--- src/zipkin.js
+++ src/zipkin.js
@@ -1,5 +1,6 @@
 // FIXME this is a nasty copy+paste hack to get the zipkin exporter able to work in a browser context.
 import {toZipkinSpan, statusCodeTagName, statusDescriptionTagName} from '../deps/opentelemetry-js/packages/opentelemetry-exporter-zipkin/src/transform';
+import {ExportResult} from '@opentelemetry/core';
 
 export class PatchedZipkinExporter {
   constructor(beaconUrl) {
@@ -12,7 +13,7 @@ export class PatchedZipkinExporter {
     if (navigator.sendBeacon) {
       navigator.sendBeacon(this.beaconUrl, zJson);
     }
-    resultCallback('SUCCESS');
+    resultCallback(ExportResult.SUCCESS);
   }
 
   shutdown() {
