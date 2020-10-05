1c12c20a8cdb29afc75ffbab36175d646088b0d5
John Bley <jbley@splunk.com>
2020-10-05 12:54:22 -0400

    Syntax check the debug build since we publish it now

--- fullbuild.sh
+++ fullbuild.sh
@@ -6,6 +6,9 @@ npm run lint
 npm test
 npx rollup -c
 DEBUG_BUILD=1 npx rollup -c
+# syntax check the result as an extra precaution
 npx eslint --no-eslintrc --env es2015 dist/splunk-rum.js
+# syntax check the debug build but ignore inline eslint stuff from otel
+npx eslint --no-eslintrc --no-inline-config --env es2015 dist/splunk-rum.debug.js
 echo ""
 echo "------- Excelsior! -------"
