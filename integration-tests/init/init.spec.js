f2ec23c2b4dbab2d874f4619895567a1de3f6121
Marten Hennoch <marten@plumbr.io>
2020-10-22 16:22:58 +0300

    Initial templating support and support for loading  different files for integration tests.

---
+++ integration-tests/init/init.spec.js
@@ -0,0 +1,9 @@
+
+// module.exports = {
+//   'init test': async function(browser) {
+//     // await browser.url('/integration-tests/init/init.html');
+//     await browser.url(`${browser.globals.defaultUrl}/init/init.html`);
+//     await browser.assert.ok(true);
+//     await browser.end();
+//   },
+// };
