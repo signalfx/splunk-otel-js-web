24bc754eddb0bdcfdbb3106c5c4083ebd8695782
John Bley <jbley@splunk.com>
2020-11-30 14:40:51 -0500

    Improved testing of docload, mostly event timestamps

--- integration-tests/tests/docload/docload.spec.js
+++ integration-tests/tests/docload/docload.spec.js
@@ -17,15 +17,26 @@ limitations under the License.
 module.exports = {
   'documentFetch, resourceFetch, and documentLoad spans': async function(browser) {
 
-    const events2hash = function(eventsArr) {
-      const answer = {};
-      if (!eventsArr) {
-        return answer;
-      }
+    const timesMakeSense = async function(eventsArr, startName, endName) {
+      const name2time = {};
       eventsArr.forEach(event => {
-        answer[event.value] = event;
+        name2time[event.value] = event.timestamp;
       });
-      return answer;
+      await browser.assert.ok(name2time[startName]);
+      await browser.assert.ok(name2time[endName]);
+      await browser.assert.ok(name2time[startName] <= name2time[endName]);
+      const diff = name2time[endName] - name2time[startName];
+      // times reported in zipkin as micros
+      // looking for egregiously incorrect response times here, not a tight bound
+      const fiveMinutes = 5 * 60 * 1000 * 1000;
+      await browser.assert.ok(diff < fiveMinutes); 
+      // Also looking for rough synchronization with reality (at least from our CI systems/laptops...)
+      const nowMicros = new Date().getTime() * 1000;
+      let clockSkew = name2time[startName] - nowMicros;
+      if (clockSkew < 0) {
+        clockSkew = -clockSkew;
+      }
+      await browser.assert.ok(clockSkew <= fiveMinutes);
     };
 
     const url = browser.globals.getUrl('/docload/docload.ejs');
@@ -38,36 +49,31 @@ module.exports = {
     await browser.assert.ok(scriptFetch);
     await browser.assert.ok(docLoad);
 
-    // FIXME need events for each too
     // docFetch
     await browser.assert.strictEqual(docFetch.tags['component'], 'document-load');
     await browser.assert.strictEqual(docFetch.tags['location.href'], url);
-    await browser.assert.ok(events2hash(docFetch.annotations).fetchStart);
-    await browser.assert.ok(events2hash(docFetch.annotations).domainLookupStart);
-    await browser.assert.ok(events2hash(docFetch.annotations).domainLookupEnd);
-    await browser.assert.ok(events2hash(docFetch.annotations).connectStart);
-    await browser.assert.ok(events2hash(docFetch.annotations).connectEnd);
-    await browser.assert.ok(events2hash(docFetch.annotations).requestStart);
-    await browser.assert.ok(events2hash(docFetch.annotations).responseStart);
-    await browser.assert.ok(events2hash(docFetch.annotations).responseEnd);
+    await timesMakeSense(docFetch.annotations, 'domainLookupStart', 'domainLookupEnd');
+    await timesMakeSense(docFetch.annotations, 'connectStart', 'connectEnd');
+    await timesMakeSense(docFetch.annotations, 'requestStart', 'responseStart');
+    await timesMakeSense(docFetch.annotations, 'responseStart', 'responseEnd');
+    await timesMakeSense(docFetch.annotations, 'fetchStart', 'responseEnd');
     // FIXME good way to know when this should be available?
-    //await browser.assert.ok(span.tags['http.response_content_length'] >= 0);
+    await browser.assert.ok(span.tags['http.response_content_length'] >= 0);
 
     // scriptFetch
     await browser.assert.strictEqual(scriptFetch.tags['component'], 'document-load');
     await browser.assert.strictEqual(scriptFetch.tags['location.href'], url);
-    //await browser.assert.ok(span.tags['http.response_content_length'] >= 0);
-
+    await browser.assert.ok(span.tags['http.response_content_length'] >= 0);
+    // http.url has already been checked by the findSpan
+    
     // documentLoad
     await browser.assert.strictEqual(docLoad.tags['component'], 'document-load');
     await browser.assert.strictEqual(docLoad.tags['location.href'], url);
     await browser.assert.ok(docLoad.tags['screen.xy'].match(/[0-9]+x[0-9]+/));
-    await browser.assert.ok(events2hash(docLoad.annotations).fetchStart);
-    await browser.assert.ok(events2hash(docLoad.annotations).domContentLoadedEventStart);
-    await browser.assert.ok(events2hash(docLoad.annotations).domContentLoadedEventEnd);
-    await browser.assert.ok(events2hash(docLoad.annotations).domComplete);
-    await browser.assert.ok(events2hash(docLoad.annotations).loadEventStart);
-    await browser.assert.ok(events2hash(docLoad.annotations).loadEventEnd);
+    await timesMakeSense(docLoad.annotations, 'domContentLoadedEventStart', 'domContentLoadedEventEnd');
+    await timesMakeSense(docLoad.annotations, 'loadEventStart', 'loadEventEnd');
+    await timesMakeSense(docLoad.annotations, 'fetchStart', 'domInteractive');
+    await timesMakeSense(docLoad.annotations, 'fetchStart', 'domComplete');
 
 
 
