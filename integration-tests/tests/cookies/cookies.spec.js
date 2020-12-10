25e0c52ffb8eb9c81c6dd8ee7958b143929ccf41
MartenH <72463136+mhennoch@users.noreply.github.com>
2020-12-10 18:59:22 +0200

    Same site cookies (#29)
    * Add correct SameSite attribute to cookie and if unable to set cookie fall back to instanceId.

---
+++ integration-tests/tests/cookies/cookies.spec.js
@@ -0,0 +1,50 @@
+/*
+Copyright 2020 Splunk Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+module.exports = {
+  afterEach : function(browser) {
+    browser.globals.clearReceivedSpans();  
+  },
+  'setting session cookie should work': async function(browser) {
+    await browser.url(browser.globals.getUrl('/cookies/cookies.ejs'));
+
+    const fetchSpan = await browser.globals.findSpan(span => span.name === 'documentFetch');
+    const cookie = await browser.getCookie('_splunk_rum_sid');
+    await browser.assert.ok(fetchSpan.tags['splunk.rumSessionId']);
+    await browser.assert.notEqual(fetchSpan.tags['splunk.scriptInstance'], fetchSpan.tags['splunk.rumSessionId']);
+    await browser.assert.ok(cookie);
+
+    if (browser.options.desiredCapabilities.browserName.toLowerCase() !==  'safari') {
+      await browser.assert.equal(cookie.sameSite, 'Strict');
+    }
+
+    await browser.end();
+  },
+  'setting session cookie in iframe should work': async function(browser) {
+    await browser.url(browser.globals.getUrl('/cookies/cookies.iframe.ejs'));
+
+    const fetchSpan = await browser.globals.findSpan(span => span.name === 'documentFetch' && span.tags.app === 'iframe');
+    const cookie = await browser.getCookie('_splunk_rum_sid');
+    await browser.assert.ok(cookie);
+    await browser.assert.ok(fetchSpan);
+    await browser.assert.ok(cookie.secure);
+    if (browser.options.desiredCapabilities.browserName.toLowerCase() !==  'safari') {
+      await browser.assert.equal(cookie.sameSite, 'None');
+    }
+
+    await browser.end();
+  }
+};
