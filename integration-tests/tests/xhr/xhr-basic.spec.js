b96d33819475ec7cdaaab1e3b58ecef0e39c0a39
John Bley <jbley@splunk.com>
2020-12-21 22:06:17 -0500

    Additional tests for xhr attributes/events

--- integration-tests/tests/xhr/xhr-basic.spec.js
+++ integration-tests/tests/xhr/xhr-basic.spec.js
@@ -14,10 +14,30 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+const specUtils = require('../spec-utils');
+
 module.exports = {
   'XHR request is registered': async function(browser) {
     await browser.url(browser.globals.getUrl('/xhr/views/xhr-basic.ejs'));
     const xhrSpan = await browser.globals.findSpan(span => span.tags['http.url'] === '/some-data');
     await browser.assert.ok(xhrSpan);
+    await browser.assert.strictEqual(xhrSpan.tags['component'], 'xml-http-request');
+    await browser.assert.strictEqual(xhrSpan.tags['http.status_code'], '200');
+    await browser.assert.strictEqual(xhrSpan.tags['http.status_text'], 'OK');
+    await browser.assert.strictEqual(xhrSpan.tags['http.method'], 'GET');
+    await browser.assert.strictEqual(xhrSpan.tags['http.url'], '/some-data');
+    if (browser.options.desiredCapabilities.browserName.toLowerCase() !==  'safari') {
+      await browser.assert.strictEqual(xhrSpan.tags['http.response_content_length'], '49');
+    }
+    await browser.assert.ok(xhrSpan.tags['link.traceId']);
+    await browser.assert.ok(xhrSpan.tags['link.spanId']);
+    await specUtils.timesMakeSense(browser, xhrSpan.annotations, 'domainLookupStart', 'domainLookupEnd');
+    await specUtils.timesMakeSense(browser, xhrSpan.annotations, 'connectStart', 'connectEnd');
+    await specUtils.timesMakeSense(browser, xhrSpan.annotations, 'secureConnectionStart', 'connectEnd');
+    await specUtils.timesMakeSense(browser, xhrSpan.annotations, 'requestStart', 'responseStart');
+    await specUtils.timesMakeSense(browser, xhrSpan.annotations, 'responseStart', 'responseEnd');
+    await specUtils.timesMakeSense(browser, xhrSpan.annotations, 'fetchStart', 'responseEnd');
+    await specUtils.timesMakeSense(browser, xhrSpan.annotations, 'open', 'send');
+    await specUtils.timesMakeSense(browser, xhrSpan.annotations, 'send', 'loaded');
   },
 };
