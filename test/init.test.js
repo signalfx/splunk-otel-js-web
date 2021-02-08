5e490737743b1ca7325997b8abf5762ceb9673b2
Marten Hennoch <mhennoch@splunk.com>
2021-02-08 21:02:15 +0200

    Fix unit test

--- test/init.test.js
+++ test/init.test.js
@@ -314,9 +314,9 @@ describe('test unloaded img', () => {
     img.src = location.href+'/IAlwaysWantToUseVeryVerboseDescriptionsWhenIHaveToEnsureSomethingDoesNotExist.jpg';
     document.body.appendChild(img);
     setTimeout(() => {
-      assert.strictEqual(capturer.spans.length, 1);
-      const span = capturer.spans[0];
-      assert.strictEqual(span.attributes.component, 'error');
+      assert.strictEqual(capturer.spans.length, 2);
+      const span = capturer.spans.find( s => s.attributes.component === 'error');
+      assert.ok(span);
       assert.strictEqual(span.name, 'eventListener.error');
       assert.ok(span.attributes.target_src.endsWith('DoesNotExist.jpg'));
 
