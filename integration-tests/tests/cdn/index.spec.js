9502cf51690fb5ed58ddbb333479df3e636ab566
Jakub Malinowski <645127+jtmalinowski@users.noreply.github.com>
2021-02-22 11:43:04 +0100

    feat: CDN release

---
+++ integration-tests/tests/cdn/index.spec.js
@@ -0,0 +1,44 @@
+/*
+Copyright 2020 Splunk Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+module.exports = {
+  'JS unhandled error': async function(browser) {
+    browser.globals.clearReceivedSpans();
+    await browser.url(browser.globals.getUrl('/cdn/index.ejs'));
+
+    const errorSpan = await browser.globals.findSpan(s => s.name === 'onerror');
+    await browser.assert.ok(!!errorSpan, 'Checking presence of error span.');
+
+    const tags = errorSpan.tags;
+    await browser.assert.strictEqual(tags['component'], 'error');
+    await browser.assert.strictEqual(tags['error'], 'true');
+    await browser.assert.strictEqual(tags['error.object'], 'TypeError');
+
+    switch (browser.options.desiredCapabilities.browserName.toLowerCase()) {
+    case 'chrome':
+      await browser.assert.strictEqual(tags['error.message'], 'Cannot set property \'prop1\' of null');
+      break;
+    case 'firefox':
+      await browser.assert.strictEqual(tags['error.message'], 'test is null');
+      break;
+    case 'safari':
+      await browser.assert.strictEqual(tags['error.message'], 'null is not an object (evaluating \'test.prop1 = true\')');
+      break;
+    }
+
+    await browser.end();
+  },
+};
