2e51528f6fae4c4811d7fd0740790e889bf402d0
t2t2 <taavot@gmail.com>
2021-03-08 17:07:39 +0200

    readme and remove a manual test

--- src/zipkin.js
+++ src/zipkin.js
@@ -59,7 +59,7 @@ export class PatchedZipkinExporter {
     spans = spans.filter(span => exporter.filter(span));
     const zspans = spans.map(span => this.modZipkinSpan(span));
     const zJson = JSON.stringify(zspans);
-    if (false && navigator.sendBeacon) {
+    if (navigator.sendBeacon) {
       navigator.sendBeacon(this.beaconUrl, zJson);
     } else {
       sendXHR(this.beaconUrl, zJson); 
