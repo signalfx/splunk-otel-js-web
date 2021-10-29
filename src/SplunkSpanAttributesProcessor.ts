41e8179c2284aacff8b4cbfad3b1781c03d4c377
t2t2 <taavot@gmail.com>
2021-10-29 15:04:48 +0300

    Move span attribute setting to spanprocessor (#279)

---
+++ src/SplunkSpanAttributesProcessor.ts
@@ -0,0 +1,58 @@
+/*
+Copyright 2021 Splunk Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import { SpanAttributes } from '@opentelemetry/api';
+import { Span, SpanProcessor } from '@opentelemetry/sdk-trace-base';
+
+export class SplunkSpanAttributesProcessor implements SpanProcessor {
+  private readonly _globalAttributes: SpanAttributes;
+
+  constructor(globalAttributes: SpanAttributes) {
+    this._globalAttributes = globalAttributes ?? {};
+  }
+
+  setGlobalAttributes(attributes: SpanAttributes): void {
+    if (attributes) {
+      Object.assign(this._globalAttributes, attributes);
+    } else {
+      for (const key of Object.keys(this._globalAttributes)) {
+        delete this._globalAttributes[key];
+      }
+    }
+  }
+
+  _experimental_getGlobalAttributes(): SpanAttributes {
+    return this._globalAttributes;
+  }
+
+  forceFlush(): Promise<void> {
+    return Promise.resolve();
+  }
+
+  onStart(span: Span): void {
+    span.setAttribute('location.href', location.href);
+    span.setAttributes(this._globalAttributes);
+  }
+
+  onEnd(): void {
+    // Intentionally empty
+  }
+
+  shutdown(): Promise<void> {
+    return Promise.resolve();
+  }
+
+}
