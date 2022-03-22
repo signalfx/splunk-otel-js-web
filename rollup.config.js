c83fd6a2e5af486834599b5a7a4282ccbe5553c5
t2t2 <taavot@gmail.com>
2022-03-22 19:04:44 +0200

    initial rr-web bundle

--- rollup.config.js
+++ rollup.config.js
@@ -68,4 +68,26 @@ export default [
     ],
     context: 'window',
   },
+  {
+    input: 'src/record/index.ts',
+    output: {
+      file: 'dist/artifacts/splunk-otel-web-record.js',
+      format: 'iife',
+      name: 'SplunkRumRecorder',
+      sourcemap: true,
+    },
+    plugins: [
+      json(),
+      nodeResolvePlugin,
+      commonjs({
+        include: /node_modules/,
+        sourceMap: true,
+        transformMixedEsModules: true,
+      }),
+      typescript({ tsconfig: './tsconfig.base.json' }),
+      babelPlugin,
+      terser({ output: { comments: false } }),
+    ],
+    context: 'window',
+  },
 ];
---
+++ src/record/index.ts
@@ -0,0 +1,61 @@
+/*
+Copyright 2022 Splunk Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import { record } from 'rrweb';
+
+type RRWebOptions = Parameters<typeof record>[0];
+
+export type SplunkRumRecorderConfig = RRWebOptions & {
+  /** Destination for the captured data */
+  beaconUrl?: string;
+
+  /** Temporary! Auth token */
+  apiToken?: string;
+}
+
+let inited: (() => void) | false = false;
+
+const SplunkRumRecorder = {
+  get inited(): boolean {
+    return Boolean(inited);
+  },
+
+  init(config: SplunkRumRecorderConfig): void {
+    if (inited) {
+      return;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { beaconUrl, ...rrwebConf } = config;
+
+    inited = record({
+      ...rrwebConf,
+      emit(event) {
+        console.log('Event from rrweb', event);
+      }
+    });
+  },
+  deinit(): void {
+    if (!inited) {
+      return;
+    }
+
+    inited();
+    inited = false;
+  }
+};
+
+export default SplunkRumRecorder;
