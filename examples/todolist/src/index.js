93f2d57ed2f80f6437dc2d0e61e7eddae874b686
t2t2 <taavot@gmail.com>
2022-05-17 14:29:01 +0300

    Update examples, react to 18 compatible

--- examples/todolist/src/index.js
+++ examples/todolist/src/index.js
@@ -15,13 +15,14 @@ limitations under the License.
 */
 
 import { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
