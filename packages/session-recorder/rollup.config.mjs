b3745e5a584dfac7e9337d0200be93032952423f
t2t2 <taavot@gmail.com>
2023-07-26 17:11:41 +0300

    Mass-update all of the dependencies

--- packages/session-recorder/rollup.config.mjs
+++ packages/session-recorder/rollup.config.mjs
@@ -2,13 +2,13 @@
 
 import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 
-const {
+import {
   babelPlugin,
   nodeResolvePlugin,
-} = require('../../rollup.shared');
+} from '../../rollup.shared.mjs';
 
 export default [
   {
