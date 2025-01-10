44b86dbf76cc618b978ac5bf33b1363124559e4a
Jozef Harag <jh.developer@pm.me>
2025-01-10 12:38:19 +0100

    chore(internal): update license headers to reflect 2025 (#920)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
 /**
  *
- * Copyright 2024 Splunk Inc.
+ * Copyright 2020-2025 Splunk Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -52,13 +52,13 @@ export default [
 					source: 'string',
 					content: [
 						'',
-						`Copyright ${new Date().getFullYear()} Splunk Inc.`,
+						`Copyright 2020-${new Date().getFullYear()} Splunk Inc.`,
 						'',
 						'Licensed under the Apache License, Version 2.0 (the "License");',
 						'you may not use this file except in compliance with the License.',
 						'You may obtain a copy of the License at',
 						'',
-						'http://www.apache.org/licenses/LICENSE-2.0',
+						'https://www.apache.org/licenses/LICENSE-2.0',
 						'',
 						'Unless required by applicable law or agreed to in writing, software',
 						'distributed under the License is distributed on an "AS IS" BASIS,',
