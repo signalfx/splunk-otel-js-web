5dacc899fd98eb45e4ff09d97032a215050d836a
Max Virgil <mvirgil@splunk.com>
2025-01-21 18:29:09 -0800

    Fix lint errors in build-plugins

--- packages/build-plugins/test/utils.test.ts
+++ packages/build-plugins/test/utils.test.ts
@@ -1,48 +1,49 @@
-/*
-Copyright 2024 Splunk Inc.
+/**
+ *
+ * Copyright 2020-2025 Splunk Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-import { expect } from 'chai';
-import { computeSourceMapId, getCodeSnippet } from '../src/utils';
+import { expect } from 'chai'
+import { computeSourceMapId, getCodeSnippet } from '../src/utils'
 
 describe('getCodeSnippet', function () {
-  it('inserts the source map id into the snippet', function () {
-    const code = getCodeSnippet('testid123');
-    expect(code).contains('window.sourceMapIds[s] = \'testid123\'');
-  });
+	it('inserts the source map id into the snippet', function () {
+		const code = getCodeSnippet('testid123')
+		expect(code).contains("window.sourceMapIds[s] = 'testid123'")
+	})
 
-  it('does not throw error in a node environment', function () {
-    const code = getCodeSnippet('testid123');
-    // eslint-disable-next-line no-eval
-    eval(code);
-  });
+	it('does not throw error in a node environment', function () {
+		const code = getCodeSnippet('testid123')
 
-  it('does not throw error when appended to code that does not end with semicolon or new line', function () {
-    const code = 'let x = 5' + getCodeSnippet('testid123');
-    // eslint-disable-next-line no-eval
-    eval(code);
-  });
+		eval(code)
+	})
 
-});
+	it('does not throw error when appended to code that does not end with semicolon or new line', function () {
+		const code = 'let x = 5' + getCodeSnippet('testid123')
+
+		eval(code)
+	})
+})
 
 describe('computeSourceMapId', function () {
-  it('returns a consistent id in GUID format', function () {
-    const id = computeSourceMapId('console.log("Hello world")');
+	it('returns a consistent id in GUID format', function () {
+		const id = computeSourceMapId('console.log("Hello world")')
 
-    expect(id).eq('d77ec5d8-4fb5-fbc8-1897-54b54e939bcd');
-    expect(id).eq(computeSourceMapId('console.log("Hello world")'));
-    expect(id).not.eq(computeSourceMapId('console.log("a different snippet gets a different id");'));
-  });
-});
+		expect(id).eq('d77ec5d8-4fb5-fbc8-1897-54b54e939bcd')
+		expect(id).eq(computeSourceMapId('console.log("Hello world")'))
+		expect(id).not.eq(computeSourceMapId('console.log("a different snippet gets a different id");'))
+	})
+})
