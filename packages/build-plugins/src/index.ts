364e02b5e94b84baeff1a6606988396a592ac88b
Max Virgil <maxtvirgil+code@gmail.com>
2025-03-04 09:38:17 -0800

    chore(build-plugins): update API and add input validation (#987)

--- packages/build-plugins/src/index.ts
+++ packages/build-plugins/src/index.ts
@@ -17,16 +17,16 @@
  */
 import { createUnplugin, UnpluginFactory } from 'unplugin'
 
-import type { WebpackPluginInstance } from 'webpack'
+import type { Compiler, WebpackPluginInstance } from 'webpack'
 import { PLUGIN_NAME } from './utils'
 import { applySourceMapsInject, applySourceMapsUpload } from './webpack'
 
-export interface OllyWebPluginOptions {
-	/** Plugin configuration for source map ID injection and source map file uploads */
-	sourceMaps: {
-		/** Optional. If provided, this should match the "applicationName" used where SplunkRum.init() is called. */
-		applicationName?: string
+export interface SplunkRumPluginOptions {
+	/** Optional. If provided, this should match the "applicationName" used where SplunkRum.init() is called. */
+	applicationName?: string
 
+	/** Plugin configuration for source map ID injection and source map file uploads */
+	sourceMaps?: {
 		/** Optional. If true, the plugin will inject source map IDs into the final JavaScript bundles, but it will not upload any source map files. */
 		disableUpload?: boolean
 
@@ -35,23 +35,53 @@ export interface OllyWebPluginOptions {
 
 		/** API token used to authenticate the file upload requests.  This is not the same as the rumAccessToken used in SplunkRum.init(). */
 		token: string
-
-		/** Optional. If provided, this should match the "version" used where SplunkRum.init() is called. */
-		version?: string
 	}
+
+	/** Optional. If provided, this should match the "version" used where SplunkRum.init() is called. */
+	version?: string
 }
 
-const unpluginFactory: UnpluginFactory<OllyWebPluginOptions | undefined> = (options) => ({
+const unpluginFactory: UnpluginFactory<SplunkRumPluginOptions | undefined> = (options) => ({
 	name: PLUGIN_NAME,
 	webpack(compiler) {
-		applySourceMapsInject(compiler)
+		const logger = compiler.getInfrastructureLogger(PLUGIN_NAME)
 
-		if (!options.sourceMaps.disableUpload) {
-			applySourceMapsUpload(compiler, options)
+		if (options.sourceMaps) {
+			applySourceMapsInject(compiler)
+
+			let shouldApplySourceMapsUpload = true
+			if (options.sourceMaps.disableUpload) {
+				logger.debug('sourceMaps.disableUpload is set — skipping source map uploads')
+				shouldApplySourceMapsUpload = false
+			} else if (!options.sourceMaps.realm) {
+				logger.warn('sourceMaps.realm is missing — skipping source map uploads')
+				shouldApplySourceMapsUpload = false
+			} else if (!options.sourceMaps.token) {
+				logger.warn('sourceMaps.token is missing — skipping source map uploads')
+				shouldApplySourceMapsUpload = false
+			}
+
+			if (shouldApplySourceMapsUpload) {
+				applySourceMapsUpload(compiler, options)
+			}
 		}
 	},
 })
 
 const unplugin = createUnplugin(unpluginFactory)
 
-export const ollyWebWebpackPlugin: (options: OllyWebPluginOptions) => WebpackPluginInstance = unplugin.webpack
+export class SplunkRumWebpackPlugin {
+	// unplugin.webpack gives us a function
+	// Wrap it in this exported class so that usages of our plugin follow the convention of other webpack plugins,
+	// which use the "new" keyword to create instances of plugins
+
+	private unpluginInstance: WebpackPluginInstance
+
+	constructor(options: SplunkRumPluginOptions) {
+		this.unpluginInstance = unplugin.webpack(options)
+	}
+
+	apply(compiler: Compiler): void {
+		this.unpluginInstance.apply(compiler)
+	}
+}
