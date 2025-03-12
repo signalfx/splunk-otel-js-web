5f599ea0b4428e79cc02a5c19cca44e5d88bc4e4
Jozef Harag <jh.developer@pm.me>
2025-03-12 08:42:48 +0100

    fix: session sampler create session

--- packages/web/src/SessionBasedSampler.ts
+++ packages/web/src/SessionBasedSampler.ts
@@ -18,7 +18,7 @@
 
 import { Context, Link, Sampler, SamplingResult, SpanAttributes, SpanKind } from '@opentelemetry/api'
 import { AlwaysOffSampler, AlwaysOnSampler } from '@opentelemetry/core'
-import { getRumSessionId } from './session'
+import { getOrCreateSessionIdAndUpdateExpirationIfNecessary, getRumSessionId } from './session'
 
 export interface SessionBasedSamplerConfig {
 	/**
@@ -78,7 +78,10 @@ export class SessionBasedSampler implements Sampler {
 		// Implementation based on @opentelemetry/core TraceIdRatioBasedSampler
 		// but replacing deciding based on traceId with sessionId
 		// (not extended from due to private methods)
-		const currentSession = getRumSessionId({ useLocalStorage: this.useLocalStorageForSessionMetadata })
+		const currentSession = getOrCreateSessionIdAndUpdateExpirationIfNecessary({
+			useLocalStorage: this.useLocalStorageForSessionMetadata,
+			forceStore: true,
+		})
 		if (this._currentSession !== currentSession) {
 			this._currentSessionSampled = this._accumulate(currentSession) < this._upperBound
 			this._currentSession = currentSession
