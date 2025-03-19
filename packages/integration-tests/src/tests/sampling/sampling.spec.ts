04a746ff04529d8deff9e240e5ed7b6b8f12792a
David Kadlec <dkadlec@splunk.com>
2025-03-19 14:36:56 +0100

    improvement: _experimental_allSpansExtendSession tests

--- packages/integration-tests/src/tests/sampling/sampling.spec.ts
+++ packages/integration-tests/src/tests/sampling/sampling.spec.ts
@@ -20,45 +20,42 @@ import { test } from '../../utils/test'
 
 test.describe('sampling', () => {
 	test('all spans arrive if ratio is 1', async ({ recordPage }) => {
-		await recordPage.goTo('/sampling/index.ejs?samplingRatio=1')
+		await recordPage.goTo('/sampling/sampling.ejs?samplingRatio=1')
 		await recordPage.waitForSpans((spans) => spans.filter((span) => span.name === 'guard-span').length === 1)
 	})
 
 	test('no spans arrive if ratio is 0', async ({ recordPage }) => {
-		await recordPage.goTo('/sampling/index.ejs?samplingRatio=0')
+		await recordPage.goTo('/sampling/sampling.ejs?samplingRatio=0')
 		await recordPage.waitForTimeout(1000)
 
 		expect(recordPage.receivedSpans).toHaveLength(0)
 	})
 
 	test('all spans arrive if session was sampled', async ({ recordPage }) => {
-		await recordPage.goTo('/sampling/index.ejs?samplingRatio=0.99&forceSessionId=a0000000000000000000000000000000')
-		await recordPage.waitForSpans((spans) => spans.filter((span) => span.name === 'guard-span').length === 1)
-	})
-
-	test('no spans arrive if session was not sampled', async ({ recordPage }) => {
-		await recordPage.goTo('/sampling/index.ejs?samplingRatio=0.01&forceSessionId=a0000000000000000000000000000000')
-		await recordPage.waitForTimeout(1000)
-
-		expect(recordPage.receivedSpans).toHaveLength(0)
-	})
-
-	test('all spans arrive if session was sampled even if longtasks do not start a new session', async ({
-		recordPage,
-	}) => {
 		await recordPage.goTo(
-			'/sampling/index.ejs?samplingRatio=0.99&forceSessionId=a0000000000000000000000000000000&_experimental_longtaskNoStartSession=true',
+			'/sampling/sampling.ejs?samplingRatio=0.99&forceSessionId=a0000000000000000000000000000000',
 		)
-
-		// spans have arrived
 		await recordPage.waitForSpans((spans) => spans.filter((span) => span.name === 'guard-span').length === 1)
 
-		// we have a cookie
 		const sessionCookieEncoded = await recordPage.getCookie('_splunk_rum_sid')
 		expect(sessionCookieEncoded).toBeTruthy()
 
-		// and the session is active
 		const sessionCookieRaw = decodeURIComponent(sessionCookieEncoded.value)
-		expect(JSON.parse(sessionCookieRaw).inactive).toBeFalsy()
+		expect(JSON.parse(sessionCookieRaw).id).toBe('a0000000000000000000000000000000')
+	})
+
+	test('no spans arrive if session was not sampled', async ({ recordPage }) => {
+		await recordPage.goTo(
+			'/sampling/sampling.ejs?samplingRatio=0.01&forceSessionId=a0000000000000000000000000000000',
+		)
+		await recordPage.waitForTimeout(1000)
+
+		expect(recordPage.receivedSpans).toHaveLength(0)
+
+		const sessionCookieEncoded = await recordPage.getCookie('_splunk_rum_sid')
+		expect(sessionCookieEncoded).toBeTruthy()
+
+		const sessionCookieRaw = decodeURIComponent(sessionCookieEncoded.value)
+		expect(JSON.parse(sessionCookieRaw).id).toBe('a0000000000000000000000000000000')
 	})
 })
