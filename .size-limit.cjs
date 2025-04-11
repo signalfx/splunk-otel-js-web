f339ee43d0e8bb6ed1525df55c4eb0f3a787fa6d
Pavel Potáček <potacek.pavel@gmail.com>
2025-04-11 11:41:56 +0200

    size

--- .size-limit.cjs
+++ .size-limit.cjs
@@ -36,7 +36,7 @@ module.exports = [
 
 	{
 		name: 'artifacts/splunk-otel-web-session-recorder.js',
-		limit: '93 kB',
+		limit: '94 kB',
 		path: './packages/session-recorder/dist/artifacts/splunk-otel-web-session-recorder.js',
 	},
 ]
