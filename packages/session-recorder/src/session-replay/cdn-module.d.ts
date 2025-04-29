e49fdf7d5fe805bed3bedf69c563390e00110929
Pavel Potáček <potacek.pavel@gmail.com>
2025-04-29 11:16:36 +0200

    fix lint

--- packages/session-recorder/src/session-replay/cdn-module.d.ts
+++ packages/session-recorder/src/session-replay/cdn-module.d.ts
@@ -1,13 +1,34 @@
+/**
+ *
+ * Copyright 2020-2025 Splunk Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 declare module 'https://cdn.signalfx.com/o11y-gdi-rum/session-replay/v1.33.0/session-replay.module.legacy.min.js' {
 	class SessionReplayBase {
-		start: () => Promise<void>
-		stop: () => void
 		static clear: () => void
+
+		start: () => Promise<void>
+
+		stop: () => void
 	}
 
 	export class SessionReplay extends SessionReplayBase {
 		constructor(config: SessionReplayConfig)
+
 		static compressData: (dataToCompress: ReadableStream<Uint8Array>, format: 'deflate' | 'gzip') => Promise<Blob>
+
 		static isCompressionSupported: () => Promise<boolean>
 	}
 
