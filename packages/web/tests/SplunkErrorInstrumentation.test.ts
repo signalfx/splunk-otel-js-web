a717f48125b84bdf642b179d361db50705ef5923
Ryan Bonte <rybon@users.noreply.github.com>
2025-07-16 23:43:31 +0200

    Trailing comma

--- packages/web/tests/SplunkErrorInstrumentation.test.ts
+++ packages/web/tests/SplunkErrorInstrumentation.test.ts
@@ -132,7 +132,7 @@ const stack6 = `Error
 const expected6 = [
 	'http://example.com:8080/path/js/main.js?name=testname',
 	'http://example.com:8080/path/js/main2.js#fragmentHere',
-	'http://example.com:8080/path/js/main3.js?name=testname#fragmentHere'
+	'http://example.com:8080/path/js/main3.js?name=testname#fragmentHere',
 ]
 
 // Test 7: Urls with different protocols and blobs
