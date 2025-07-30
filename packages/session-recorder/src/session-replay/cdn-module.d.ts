15cec98a32aa0367093cf1cd0add14060c7c1904
David Kadlec <dkadlec@splunk.com>
2025-07-30 08:45:06 +0200

    chore: update session replay package

--- packages/session-recorder/src/session-replay/cdn-module.d.ts
+++ packages/session-recorder/src/session-replay/cdn-module.d.ts
@@ -15,7 +15,7 @@
  * limitations under the License.
  *
  */
-declare module 'https://cdn.signalfx.com/o11y-gdi-rum/session-replay/v2.2.4/session-replay.module.legacy.min.js' {
+declare module 'https://cdn.signalfx.com/o11y-gdi-rum/session-replay/v2.3.1/session-replay.module.legacy.min.js' {
 	type DeepPartial<T> = {
 		[P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P]
 	}
@@ -128,7 +128,7 @@ declare module 'https://cdn.signalfx.com/o11y-gdi-rum/session-replay/v2.2.4/sess
 		'browser.instance.id': string
 		'endUnixMs': number
 		'format': 'plain' | 'binary' | 'binary-deflate'
-		'lastKnowSegmentEndUnixMs': number | null
+		'lastKnownSegmentEndUnixMs': number | null
 		'sdkVersion': string
 		'source': 'web'
 		'startUnixMs': number
