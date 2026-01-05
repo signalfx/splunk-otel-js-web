ccb614148bad5a2eafac440784d66c3bb51d8b74
Jozef Harag <jh.developer@pm.me>
2026-01-05 11:01:15 +0100

    chore: update year in headers (#1556)

--- packages/integration-tests/src/tests/errors/errors.spec.ts
+++ packages/integration-tests/src/tests/errors/errors.spec.ts
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright 2020-2025 Splunk Inc.
+ * Copyright 2020-2026 Splunk Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -200,7 +200,7 @@ test.describe('errors', () => {
 		expect(
 			errorSpans3[2].tags['error.source_map_ids'] as string,
 			'this expect must be kept in-sync with the sourceMapId injected into build-plugins/tests/project/dist/webpack-config-devtool-source-map-js/main.js',
-		).toContain('acb44e4d-1860-4e5e-bd63-77d0e9f02c51')
+		).toContain('7959733f-a8c7-5e9f-e1ef-f6c82bc825cf')
 	})
 
 	test('module error', async ({ recordPage }) => {
